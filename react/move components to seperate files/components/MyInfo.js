a33c3d7b967bdc8adaa211cf37ab89a1cc79260e
Hari G <harikrishnag413@gmail.com>
2019-03-01 07:58:19 +0530

    react components
    react components

---
+++ react/move components to seperate files/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MyInfo() {
+  return(
+    <div>
+    <h1> Hellow World</h1>
+    <p> this is paragraph... </p>
+    <ul>
+      <li>india</li>
+      <li>USA</li>
+      <li>Australia</li>
+      <li>UK</li>
+    </ul>
+    </div>
+
+  )
+
+}
+export default MyInfo
