2ec6f416adb7c2c2e7e2b6e1e0b53a5b82e4929a
Hari G <harikrishnag413@gmail.com>
2019-03-02 15:21:25 +0530

    react
    react

---
+++ react/TodoApp/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TodoItems from "./components/TodoItem"
+
+
+function App() {
+  return(
+    <div className="todo-list" >
+        <TodoItems />
+        <TodoItems />
+        <TodoItems />
+        <TodoItems />
+    </div>
+
+  )
+
+}
+export default App
