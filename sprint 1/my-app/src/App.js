73e6109f033bd6dfe72a586c66c9cf765fd3b1dd
Hari G <harikrishnag413@gmail.com>
2019-03-12 19:14:11 +0530

    sprint 1
    sprint 1

---
+++ sprint 1/my-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        
+
+        </header>
+        <body className="App-body">
+
+        </body>
+        <footer className="App-footer">
+
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ sprint 1/my-app/src/App0.js
@@ -0,0 +1,32 @@
+import React, {component} from 'react'
+import Conditional from './Conditional'
+
+class App extends Component {
+
+  constructor(){
+      super()
+      this.state = {
+        isLoading: true
+      }
+  }
+
+  componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          isLoading :false
+        })
+      },1500)
+  }
+
+  render(){
+
+     return(
+           <div>
+               <Conditional isLoading={this.state.isLoading}/>
+          </div>
+       )
+   }
+}
+
+
+export default App
